de65decc1efddab2527bc543a6dd63de07dd13e4
aaarora <aaarora@ucsd.edu>
2021-07-13 12:25:08 -0700

    trying w/o supervisor

--- fts/docker/startup.sh
+++ fts/docker/startup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# initialise / upgrade the database
 /usr/share/fts/fts-database-upgrade.py <<< y
 
 /usr/bin/sed -i 's/Listen 80/#Listen 80/g' /etc/httpd/conf/httpd.conf
 
-exec /usr/bin/supervisord -c /etc/supervisord.conf
+# startup the FTS services
+/usr/sbin/fts_server               # main FTS server daemonizes
+/usr/sbin/httpd -DFOREGROUND       # FTS REST frontend & FTSMON
