6db547407187e2985929935cd7a5077a9be417fb
aaarora <aaarora@ucsd.edu>
2021-07-14 11:08:54 -0700

    fixing certs

--- fts/docker/startup.sh
+++ fts/docker/startup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# make certs with right DNS
+openssl req -new -newkey rsa:2048 -nodes -keyout /etc/grid-security/hostkey.pem -subj "/CN=fts" > /etc/grid-security/hostcert_fts.csr
+openssl x509 -req -days 9999 -CAcreateserial -extfile <(printf "subjectAltName=DNS:fts,DNS:$(hostname --fqdn)") -in /etc/grid-security/hostcert_fts.csr -CA /etc/grid-security/rucio_ca.pem -CAkey /etc/grid-security/rucio_ca.key.pem -out /etc/grid-security/hostcert.pem -passin env:123456
+mv /etc/grid-security/rucio_ca.pem /etc/grid-security/certificates/5fca1cb1.0
+chmod 400 /etc/grid-security/hostkey.pem
+
 # wait for MySQL readiness
 /usr/local/bin/wait-for-it.sh -h $DB_ADDR -p 3306 -t 3600
 
