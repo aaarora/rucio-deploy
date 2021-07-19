e72fd56226c1a5601f9fc67747a3d286a768674c
aaarora <aaarora@ucsd.edu>
2021-07-19 00:01:27 -0700

    update

--- build-deploy.sh
+++ build-deploy.sh
@@ -2,16 +2,16 @@
 
 # deploy dbs
 kubectl apply -k ./fts-db/
-sleep 5;
+sleep 20;
 kubectl apply -k ./rucio-db/
-sleep 5;
+sleep 20;
 # deploy fts
 ./fts/k8s/makeDeploy
 kubectl apply -k ./fts/k8s/
-sleep 5;
+sleep 20;
 # deploy rucio
 ./rucio/k8s/makeDeploy
 kubectl apply -k ./rucio/k8s/
-sleep 5;
+sleep 20;
 # deploy xrootd
 kubectl apply -k ./xrootd/
