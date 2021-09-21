be104f0ce7aec30f1f5faa97b829d855a0bb9910
aaarora <aaarora@ucsd.edu>
2021-09-21 12:08:47 -0700

    fixed osg-ca-gen

--- fts/docker/startup.sh
+++ fts/docker/startup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# make certs with right DNS
-openssl req -new -newkey rsa:2048 -nodes -keyout /etc/grid-security/hostkey.pem -subj "/CN=$OSG_FQDN" > /etc/grid-security/hostcert_fts.csr
-openssl x509 -req -days 9999 -CAcreateserial -extfile <(printf "subjectAltName=IP:$OSG_FQDN,DNS:localhost") -in /etc/grid-security/hostcert_fts.csr -CA /etc/grid-security/certificates/rucio_ca.pem -CAkey /etc/grid-security/certificates/rucio_ca.key.pem -out /etc/grid-security/hostcert.pem -passin pass:123456
-cp /etc/grid-security/certificates/rucio_ca.pem /etc/grid-security/certificates/hostca.crt
-chmod 400 /etc/grid-security/hostkey.pem
+# make certs 
+osg-ca-generator --host
 
 # wait for MySQL readiness
 /usr/local/bin/wait-for-it.sh -h $DB_ADDR -p 3306 -t 3600
